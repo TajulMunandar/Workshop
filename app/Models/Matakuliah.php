9646fe59b2f4cb99e52f501afbe1079ac0061e44
syahsuri <officialsyahsury@gmail.com>
2023-01-12 14:12:32 +0700

    matakuliah

--- app/Models/Matakuliah.php
+++ app/Models/Matakuliah.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Matakuliah extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id'];
+
+    public function penjualanDetails()
+    {
+        return $this->belongsTo(PenjualanDetail::class);
+    }
 }
